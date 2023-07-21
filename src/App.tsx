f45f6d7d8a334a289f75ef1d4682276817aaafc3
ianMuchesia <ianmuchesia@yahoo.com>
2023-07-21 18:29:43 +0300

    vertical_line

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Hero, Navbar } from "./components"
+import Experience from "./components/Experience"
 
 
 const App = () => {
@@ -6,6 +7,7 @@ const App = () => {
     <>
     <Navbar/>
     <Hero/>
+    <Experience/>
     </>
   )
 }
